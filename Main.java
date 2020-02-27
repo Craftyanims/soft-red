9df0c18906e2053bb31438428aa6383d7107dd7b
Dylan-Wheeler <dylan.wheeler1@ucalgary.ca>
2020-02-27 14:10:30 -0700

    Added tag, changed screen dimensions, added some basic abstract methods

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/**
+ * Author: Soft Red (Jeffery Yang, Dylan Wheeler, Steven Loppe, Thien-Kim Nguyen, Hafsa Zia) - 2020
+ * Created for SENG 300 at The University of Calgary during the winter semester of 2020
+ */
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -27,14 +32,14 @@ public class Main extends Application{
 
         TextField login_tf = new TextField();
 
-        Button login_b = new Button("Login");
+        Button login_b = new Button("Press Me");
         // TODO: set this to have logic based on the account given as input
         login_b.setOnAction(e -> {
             primaryStage.setScene(researcher_scene);
         });
 
         login_layout.getChildren().addAll(login_tf, login_b);
-        login_scene = new Scene(login_layout, 600, 800);
+        login_scene = new Scene(login_layout, 800, 600);
 
 
         /*
@@ -45,7 +50,7 @@ public class Main extends Application{
         Label researcher_l = new Label("Researcher");
 
         researcher_layout.getChildren().addAll(researcher_l);
-        researcher_scene = new Scene(researcher_layout, 600, 800);
+        researcher_scene = new Scene(researcher_layout, 800, 600);
 
 
         /*
@@ -56,7 +61,7 @@ public class Main extends Application{
         Label reviewer_l = new Label("Reviewer");
 
         reviewer_layout.getChildren().addAll(reviewer_l);
-        reviewer_scene = new Scene(reviewer_layout, 600, 800);
+        reviewer_scene = new Scene(reviewer_layout, 800, 600);
 
 
         /*
@@ -67,11 +72,28 @@ public class Main extends Application{
         Label editor_l = new Label("Editor");
 
         editor_layout.getChildren().addAll(editor_l);
-        editor_scene = new Scene(editor_layout, 600, 800);
+        editor_scene = new Scene(editor_layout, 800, 600);
 
 
         primaryStage.setScene(login_scene);
         primaryStage.show();
+    }
+
+    public User[] getAccounts() {
+        // Gets accounts from a file and returns them all as an array
+        // TODO: read accounts from a separate file, call methods in each user class to get values as objects
+        User[] accounts = new User[3];
+
+        return accounts;
+    }
+
+    public void addAccount(User aUser) {
+        // TODO: access accounts file and save this user to a new line
+
+    }
+
+    public void removeAccount(User aUser) {
+        // Should this take the user or something like an ID or index
     }
 
 }
--- Reviewer.java
+++ Reviewer.java
@@ -1,7 +1,21 @@
+/**
+ * Author: Soft Red (Jeffery Yang, Dylan Wheeler, Steven Loppe, Thien-Kim Nguyen, Hafsa Zia) - 2020
+ * Created for SENG 300 at The University of Calgary during the winter semester of 2020
+ */
+
 public class Reviewer extends User {
 
     public Reviewer(String name, String pass) {
         super(name, pass);
+    }
+
+    @Override
+    public String getType() {
+        return "Reviewer";
+    }
+
+    public String toString() {
+        return "";
     }
 
 }
