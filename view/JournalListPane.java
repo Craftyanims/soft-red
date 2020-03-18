42803b5d6cb7ddb773a0dc904d0c36b287168a0b
CraftyAnims <jeffrey7553@gmail.com>
2020-03-17 22:22:52 -0600

    download journal

--- view/JournalListPane.java
+++ view/JournalListPane.java
@@ -14,44 +14,73 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
+import javafx.stage.FileChooser;
+
+import java.nio.file.Files;
+
+import java.io.File;
+import java.io.IOException;
 
 import model.Journal;
 
 public class JournalListPane extends BasePane {
 
-	private ArrayList<model.Journal> createFakeJournals() {
-		ArrayList<model.Journal> fake = new ArrayList<model.Journal>();
-		fake.add(new model.Journal("a fake journal"));
-		fake.add(new model.Journal("another fake journal"));
-		fake.add(new model.Journal("a third fake journal"));
-		fake.add(new model.Journal("a final fake journal"));
-		
-		return fake;
-	}
-	
-	public JournalListPane(Stage stage) {
-		super(stage, "Journal List Page");
-		
-		ArrayList<model.Journal> journals = createFakeJournals();
-		
-		VBox contentPane = new VBox();
-		
-		for (int i = 0; i < journals.size(); i++) { 
-			Pane journalBox = generateJournalListItem(journals.get(i));
-			journalBox.setStyle("-fx-background-color: pink; -fx-padding: 10px;");
-			contentPane.getChildren().addAll(journalBox);
-		}
-		this.setCenter(contentPane);
-	}
-	
-	public Pane generateJournalListItem(model.Journal journal) {
-		HBox container = new HBox();
-		
-		Label titleLabel = new Label(journal.name);
-		Button viewButton = new Button("View");
-		
-		container.getChildren().addAll(titleLabel, viewButton);
-		
-		return container;
-	}
+    private Stage stage;
+
+    private ArrayList<model.Journal> createFakeJournals() {
+        ArrayList<model.Journal> fake = new ArrayList<model.Journal>();
+        fake.add(new model.Journal("sample_1"));
+        fake.add(new model.Journal("sample_2"));
+        fake.add(new model.Journal("sample_3"));
+
+        return fake;
+    }
+
+
+    public JournalListPane(Stage stage) {
+        super(stage, "Journal List Page");
+        this.stage = stage;
+
+        ArrayList<model.Journal> journals = createFakeJournals();
+
+        VBox contentPane = new VBox();
+
+        for (int i = 0; i < journals.size(); i++) {
+            Pane journalBox = generateJournalListItem(journals.get(i));
+            journalBox.setStyle("-fx-background-color: pink; -fx-padding: 10px;");
+            contentPane.getChildren().addAll(journalBox);
+        }
+        this.setCenter(contentPane);
+    }
+
+    public Pane generateJournalListItem(model.Journal journal) {
+        HBox container = new HBox();
+
+        Label titleLabel = new Label(journal.name);
+        Button viewButton = new Button("View");
+        viewButton.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save Journal");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.pdf"));
+            fileChooser.setInitialFileName(journal.name);
+            File file = new File("All Journals\\" + journal.name + ".pdf");
+
+            if (file != null) {
+                File dest = fileChooser.showSaveDialog(stage);
+                if (dest != null) {
+                    try {
+                        Files.copy(file.toPath(), dest.toPath());
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            } else {
+                System.out.println("file not found");
+            }
+        });
+
+        container.getChildren().addAll(titleLabel, viewButton);
+
+        return container;
+    }
 }
