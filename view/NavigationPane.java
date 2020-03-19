336c7311da6c43bf9f860e0ce49110a1c0cd6c0f
steven <stevenloppe>
2020-03-19 16:28:49 -0600

    Update navigationPane to use the new global Navigation object

--- view/NavigationPane.java
+++ view/NavigationPane.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
+import global.Navigation;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -15,8 +18,7 @@ public class NavigationPane extends VBox {
 
 	private HashMap<String, Class<? extends Pane>> links = new HashMap<String, Class<? extends Pane>> ();
 	
-	
-	public NavigationPane(Stage primaryStage) {
+	public NavigationPane(Stage primaryStage)  {
 		links.put("Journal List", JournalListPane.class);
 		links.put("Login", LoginPane.class);
 		links.put("Researcher", ResearcherPane.class);
@@ -27,46 +29,20 @@ public class NavigationPane extends VBox {
 		links.forEach((name, pane) -> {
 			Button button = new Button(name);
 			
-			button.setOnAction(e -> {
-				try {
-					Constructor constructor = pane.getConstructor(Stage.class);
-					Pane newPane = (Pane) constructor.newInstance(primaryStage);
-					Scene newScene = generateSceneForPane(newPane);
-					primaryStage.setScene(newScene);
-			        //primaryStage.show();	
-				}
-				catch(NoSuchMethodException exception) {
-					// Swallowing exceptions is bad but I think it is impossible for a
-					// subclass of Pane not to have a constructor that takes a Stage object.
-					// Which would make this safe to do...
-				}
-				catch(InvocationTargetException exception) {
-					// Swallowing exceptions is bad but I think it is impossible for a
-					// subclass of Pane not to have a constructor that takes a Stage object.
-					// Which would make this safe to do...
-				} catch (InstantiationException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IllegalAccessException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IllegalArgumentException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-	        });
+			button.setOnAction(event -> {
+                    try {
+						Navigation.navigate(pane);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                }
+            );
 			
 			getChildren().addAll(button);
 		});
 		
-		
 		setStyle("-fx-background-color: cyan");
-	}
-	
-	private Scene generateSceneForPane(Pane pane) {
-		Scene scene = new Scene(pane, 600, 600);
-        return scene;
 	}
 	
 }
