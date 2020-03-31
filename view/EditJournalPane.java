a9b4d757429f9231e8d13fd9a684754f176c4e23
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2020-03-31 16:28:55 -0600

    Update EditJournalPane.java

--- view/EditJournalPane.java
+++ view/EditJournalPane.java
@@ -4,6 +4,7 @@ import global.Navigation;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
