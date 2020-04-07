4fceb7413c874e96cbdcf3a668047c49d204d6fd
CraftyAnims <jeffrey7553@gmail.com>
2020-04-07 16:30:10 -0600

    minor fix

--- model/Reviewer.java
+++ model/Reviewer.java
@@ -1,11 +1,13 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Reviewer extends User implements Serializable {
-
+	private ArrayList<Journal>toReview;
 	public Reviewer(String name, String password) throws Exception {
 		super(name, password);
+		toReview = new ArrayList<Journal>();
 	}
 	
 }
